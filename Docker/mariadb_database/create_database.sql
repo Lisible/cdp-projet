0d692bcc73eea92de18060ff8287f0e91fea7a43
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-08 22:35:24 +0100

    Script de création de la table Issue

--- Docker/mariadb_database/create_database.sql
+++ Docker/mariadb_database/create_database.sql
@@ -1,9 +1,18 @@
-CREATE TABLE project (id INT NOT NULL AUTO_INCREMENT,
-                      name VARCHAR(50) NOT NULL,
-                      description VARCHAR(500),
-                      sprintDuration INT NOT NULL,
-                      beginDate DATE NOT NULL,
-                      PRIMARY KEY (id));
+CREATE TABLE Project (projectId INT NOT NULL AUTO_INCREMENT,
+                      projectName VARCHAR(50) NOT NULL,
+                      projectDescription VARCHAR(500),
+                      projectSprintDuration INT NOT NULL,
+                      projectBeginDate DATE NOT NULL,
+                      PRIMARY KEY (projectId));
+
+
+CREATE TABLE Issue (issueId INT NOT NULL AUTO_INCREMENT,
+                    issueDescription VARCHAR(500) NOT NULL,
+                    issuePriority ENUM('very low', 'low', 'medium', 'high', 'very high'),
+                    issueDifficulty INT,
+                    projectId INT NOT NULL,
+                    PRIMARY KEY (issueId),
+                    CONSTRAINT fkIssueProject FOREIGN KEY (projectId) REFERENCES Project(projectId));
 
 
                       
