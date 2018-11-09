04237e2c5da4144b026ed2d6e605b73d52c6171c
jefseutin <jeffrey.seutin@gmail.com>
2018-11-09 08:49:28 +0100

    updating backlog access

--- src/list_backlog.php
+++ src/list_backlog.php
@@ -9,7 +9,7 @@
     }
 
     public function onGet(){
-      $this->Data('backlog', DAO::getBacklog($GET["project_id"]));
+      $this->Data('backlog', DAO::getUserStory($GET["project_id"]));
       $this->render('list_backlog_view');
     }
 
