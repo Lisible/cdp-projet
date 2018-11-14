20107a0ede434f9f2303be105d9a114aeb546f34
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-14 18:53:23 +0100

    Ajout du test pour le scenario_us3

---
+++ tests_e2e/scenario_us3.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("http://localhost/project_list.php")
+assert "Liste des projets" in driver.title
+elem = driver.find_element_by_id("add_project_link")
+elem.click()
+assert "Ajouter un projet" in driver.title
+elem = driver.find_element_by_id("name-input")
+elem.click()
+elem.send_keys("TestProject")
+elem = driver.find_element_by_id("content")
+elem.click()
+elem.send_keys("Description")
+elem = driver.find_element_by_id("dureeSprint-input")
+elem.click()
+elem.send_keys("3")
+elem = driver.find_element_by_id("date-input")
+elem.click()
+elem.send_keys("11/07/2019")
+elem.submit()
+time.sleep(1)
+assert "Liste des projets" in driver.title
+elems = driver.find_elements(By.XPATH, '//li/span[text()="TestProject"]')
+assert len(elems) == 1 
+driver.close()
