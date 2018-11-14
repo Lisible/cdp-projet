127dd89e84868f1ccb3eee2f856a62e4d5620054
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-14 19:06:52 +0100

    Added .travis.yml

--- tests_e2e/scenario_us3.py
+++ tests_e2e/scenario_us3.py
@@ -1,9 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
 driver.get("http://localhost/project_list.php")
 assert "Liste des projets" in driver.title
 elem = driver.find_element_by_id("add_project_link")
