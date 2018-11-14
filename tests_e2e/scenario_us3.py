1c9575593dbe6d9dd15ae22ac1612bd878a96871
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-14 19:46:36 +0100

    Update travis and test

--- tests_e2e/scenario_us3.py
+++ tests_e2e/scenario_us3.py
@@ -1,12 +1,14 @@
 import time
+from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-options = Options()
-options.add_argument('-headless')
-driver = webdriver.Firefox(options=options)
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+driver = webdriver.Firefox()
 driver.get("http://localhost/project_list.php")
 assert "Liste des projets" in driver.title
 elem = driver.find_element_by_id("add_project_link")
