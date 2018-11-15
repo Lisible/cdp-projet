128759cc16209b4b9c146734f5a91107abf09e21
jefseutin <jeffrey.seutin@etu.u-bordeaux.fr>
2018-11-15 09:35:51 +0000

    finishing view add us

--- src/view/add_user_story_view.php
+++ src/view/add_user_story_view.php
@@ -3,9 +3,17 @@
       <label>ID* </label>
       <input type="number" name="id" id="id_us" min="1" step="1" placeholder="Identifiant de l'US" required>
       <br>
+      <label>Difficulté*</label>
+      <input type="number" name="difficulty" id="difficulty" min="1" step="1" placeholder="Difficulté de l'US" required>
+      <br>
+      <label>Priorité*</label>
+      <input type="radio" name="priority" id="priority_h" value="high">Haute
+      <input type="radio" name="priority" id="priority_m" value="medium" checked>Moyenne
+      <input type="radio" name="priority" id="priority_l" value="low">Basse
+      <br>
       <label>Description*</label>
                 <textarea name="description" rows="5" cols="50" id="description"  
-                          placeholder="Description de votre US"></textarea>
+                          placeholder="Description de votre US" required></textarea>
       <br>
      <button type="submit">Valider</input>
      <button type="button" onclick="document.location.href = '../list_backlog.php'">Annuler</button>
