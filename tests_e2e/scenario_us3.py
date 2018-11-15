ff8379f7f9ff77f3d3d968f22f82ba159193e1c3
Clément Sibille <clement.sibille@protonmail.com>
2018-11-15 10:56:32 +0100

    Correction test

--- tests_e2e/scenario_us3.py
+++ tests_e2e/scenario_us3.py
@@ -4,10 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+#display = Display(visible=0, size=(800, 600))
+#display.start()
 
 driver = webdriver.Firefox()
 driver.get("http://localhost/project_list.php")
@@ -31,7 +32,6 @@ driver.execute_script("document.querySelector('input[type=\"date\"]').valueAsDat
 elem = driver.find_element(By.XPATH, '//form')
 elem.submit()
 driver.get("http://localhost/project_list.php")
-assert "Liste des projets" in driver.title
 elems = driver.find_elements(By.XPATH, '//li/span[text()="TestProject"]')
 assert len(elems) > 0
 driver.close()
