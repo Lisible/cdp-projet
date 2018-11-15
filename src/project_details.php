60e1f88289a436b50e242219f51fd5effd84516b
Michel Massamiri <massamiri.michel@gmail.com>
2018-11-15 09:40:46 +0100

    Tc_imp : page consultation d'un projet

---
+++ src/project_details.php
@@ -0,0 +1,25 @@
+<?php
+include_once('framework/controller.php');
+include_once('model/dao.php');
+
+new class extends Controller {
+
+    public function setup() {
+        $this->setData('title', 'Les détails de ce projet :');
+    }
+
+    public function onGet($getData) {
+        $project = DAO::getProjectById($getData['project_id']);
+        if($project == null) {
+            $this->render('not_found');
+            return 1;
+        }
+
+        $this->setData('project', $project);
+        $this->render('project_details_view');
+    }
+
+    public function onPost($postData) {
+        $this->render('project_details_view');
+    }
+};
