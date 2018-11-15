15b3f37ad60f7ba011c7acabc24909ffb17047ce
Clément Sibille <clement.sibille@protonmail.com>
2018-11-15 11:19:03 +0100

    Correction test

--- tests_e2e/scenario_us3.py
+++ tests_e2e/scenario_us3.py
@@ -12,8 +12,14 @@ display.start()
 
 driver = webdriver.Firefox()
 driver.get("http://localhost/project_list.php")
+wait = WebDriverWait(driver, timeout=10)
+print(driver.title)
+assert "Liste des projets" in driver.title
 elem = driver.find_element_by_id("add_project_link")
 elem.click()
+wait = WebDriverWait(driver, timeout=10)
+print(driver.title)
+assert "Ajouter un projet" in driver.title
 elem = driver.find_element_by_id("name-input")
 elem.click()
 elem.send_keys("TestProject")
@@ -29,7 +35,12 @@ driver.execute_script("document.querySelector('input[type=\"date\"]').valueAsDat
 
 elem = driver.find_element(By.XPATH, '//form')
 elem.submit()
+
+wait = WebDriverWait(driver, timeout=10)
 driver.get("http://localhost/project_list.php")
+print(driver.title)
+assert "Liste des projets" in driver.title
+wait = WebDriverWait(driver, timeout=10)
 elems = driver.find_elements(By.XPATH, '//li/span[text()="TestProject"]')
 assert len(elems) > 0
 driver.close()
