af3550659785e43cd1519dcef4b289b8b8bb29ae
Clément SIBILLE <claymeuns@protonmail.com>
2018-11-19 13:15:58 +0100

    Ajout du test e2e pour l'US1

---
+++ tests_e2e/scenario_us1.py
@@ -0,0 +1,25 @@
+import time
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+driver = webdriver.Firefox()
+driver.get("http://localhost/index.php")
+elem = driver.find_element(By.XPATH, '//input[@name=\"username\"]')
+elem.click()
+elem.send_keys("root")
+elem = driver.find_element(By.XPATH, '//input[@name=\"password\"]')
+elem.click()
+elem.send_keys("root")
+elem = driver.find_element(By.XPATH, '//input[@type=\"submit\"]')
+elem.click()
+wait = WebDriverWait(driver, timeout=10)
+assert "Liste des projets" in driver.title
+driver.close()
