2b9bc9a9de9dcb91fd997a70f7c49cc6a28d7bd0
Michel Massamiri <massamiri.michel@gmail.com>
2018-11-21 21:37:53 +0100

    Task Th_imp

--- src/view/list_sprints_of_project_view.php
+++ src/view/list_sprints_of_project_view.php
@@ -1,3 +1,6 @@
+<div id="hidden_form_container" style="display:none;"></div>
+<button class="btn btn-primary" type="button" id="add-sprint">Ajouter un sprint</button>
+
 <?php
 $sprints = $DATA['sprints'];
 $project_id = $DATA['project_id'];
@@ -15,3 +18,22 @@ if($sprints != null): ?>
 <?php endif; ?>
 
 <a href="../project_details.php?project_id=<?php echo $project_id?>" class="btn btn-primary">Retour</a>
+
+<script defer>
+    document.getElementById('add-sprint').addEventListener("click", function () {
+        let theForm, newInput1;
+        theForm = document.createElement('form');
+        theForm.action = 'list_sprints_of_project.php';
+        theForm.method = 'post';
+
+        newInput1 = document.createElement('input');
+        newInput1.type = 'hidden';
+        newInput1.name = 'project_id';
+        newInput1.value = <?php echo $project_id ?>;
+
+        theForm.appendChild(newInput1);
+        document.getElementById('hidden_form_container').appendChild(theForm);
+
+        theForm.submit();
+    });
+</script>
