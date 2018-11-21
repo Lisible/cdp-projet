bc8f4b7f749b16e9a7f9da02e4689e014b007963
Jeffrey Seutin <44049661+jefseutin@users.noreply.github.com>
2018-11-21 17:29:07 +0100

    chaosmonkeying :[

--- tests_e2e/scenario_us8.py
+++ tests_e2e/scenario_us8.py
@@ -20,4 +20,39 @@ elem.click()
 wait = webDriverWait(driver, timeout=10)
 assert "Liste des US" in driver.title
 
+elem = driver.find_element_by_id("add_user_story_link")
+elem.click()
+wait = webDriverWait(driver, timeout=10)
+assert "Création d'une user stroy" in driver.title
+
+elem = driver.find_element_by_id("number_us")
+elem.click()
+elem.send_keys("1")
+
+elem = driver.find_element_by_id("priority_h")
+elem.click()
+
+elem = driver.find_element_by_id("difficulty")
+elem.click()
+elem.send_keys("1")
+
+elem = driver.find_element_by_id("description")
+elem.click()
+elem.send_keys("description test")
+
+elem = driver.find_element(By.XPATH, '//form')
+elem.submit()
+
+wait = WebDriverWait(driver, timeout=10)
+driver.get("http://localhost/project_list.php")
+wait = webDriverWait(driver, timeout=10)
+assert "Liste des projets" in driver.title
+
+elem = driver.find_element_by_id("project-backlog")
+elem.click()
+wait = webDriverWait(driver, timeout=10)
+assert "Liste des US" in driver.title
+elems = driver.find_elements_by_css_selector("li")
+assert len(elems) > 0
+
 driver.close()
