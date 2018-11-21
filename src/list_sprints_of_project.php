8a3ee0d9568e3b1860d6baecc245c31fd4b27eb1
Michel Massamiri <massamiri.michel@gmail.com>
2018-11-21 20:07:28 +0100

    Task Tg_imp : List sprint controller

---
+++ src/list_sprints_of_project.php
@@ -0,0 +1,22 @@
+<?php
+include_once('framework/controller.php');
+include_once('model/dao.php');
+
+new class extends Controller {
+
+    public function setup() {
+        $this->connectedCheck();
+
+        $this->setData('title', 'Liste des sprints');
+    }
+
+    public function onGet($getData){
+        $this->setData('project_id', $getData['project_id']);
+        $this->setData('sprints', DAO::getSprintsByProjectId($getData['project_id']));
+        $this->render('list_sprints_of_project_view');
+    }
+
+    public function onPost($postData){
+        $this->render('list_sprints_of_project_view');
+    }
+};
