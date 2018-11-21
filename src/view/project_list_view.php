68f9d574d8d0e40f3dd35cfe770eac989ae24d91
Jeffrey Seutin <44049661+jefseutin@users.noreply.github.com>
2018-11-21 17:08:07 +0100

    adding id for backlog button

--- src/view/project_list_view.php
+++ src/view/project_list_view.php
@@ -6,7 +6,8 @@
     <li class="list-group-item">
     	<a href="<?php echo 'project_details.php?project_id=' . $project->getId(); ?>"
             id="project-name"><?php echo $project->getName();?></a>
-    	<a href="<?php echo 'list_backlog.php?project_id=' . $project->getId(); ?>" class="btn btn-info btn-xs">
+    	<a href="<?php echo 'list_backlog.php?project_id=' . $project->getId(); ?>" class="btn btn-info btn-xs"
+           id="project-backlog">
             Backlog</a>
     </li>
     <?php endforeach; ?>
