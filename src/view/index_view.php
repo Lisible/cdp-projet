3231e69ea683df4fcf24f1844ea522119a4e7539
Jeffrey Seutin <44049661+jefseutin@users.noreply.github.com>
2018-12-07 14:23:19 +0100

    Update index_view.php

--- src/view/index_view.php
+++ src/view/index_view.php
@@ -11,3 +11,16 @@ $errorMessage = $DATA['errorMessage'];
 <?php if(isset($DATA['message'])): ?>
 <span><?php echo $DATA['message']; ?></span>
 <?php endif; ?>
+
+<a href="registration.php">Inscription</a>
+<form action="index.php" method="POST">
+    <div>
+        <label for="username">Nom d'utilisateur:</label>
+        <input name="username" type="text" required>
+    </div>
+    <div>
+        <label for="password">Mot de passe:</label>
+        <input name="password" type="password" required>
+    </div>
+     <input type="submit" value="Connexion">
+</form>
