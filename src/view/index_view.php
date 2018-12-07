e26cb56377a1caa4dc54b299a329ef06546ef8ac
Michel Massamiri <massamiri.michel@gmail.com>
2018-12-07 12:46:26 +0100

    Sonar : Factorising code for index_view

--- src/view/index_view.php
+++ src/view/index_view.php
@@ -1,24 +1,13 @@
 Index
+<?php
+$errorMessage = $DATA['errorMessage'];
+?>
 
-<?php if(isset($DATA['errorMessage'])): ?>
-<!--<span><?php echo $DATA['errorMessage']; ?></span>--!>
-<script defer>alert("<?php echo $DATA['errorMessage'];  ?>");</script>
+<?php if(isset($errorMessage)): ?>
+<!--<span><?php echo $errorMessage; ?></span>--!>
+<script defer>alert("<?php echo $errorMessage;  ?>");</script>
 <?php endif; ?>
 
 <?php if(isset($DATA['message'])): ?>
 <span><?php echo $DATA['message']; ?></span>
 <?php endif; ?>
-
-<a href="registration.php">Inscription</a>
-<form action="index.php" method="POST">
-    <div>
-        <label for="username">Nom d'utilisateur:</label>
-        <input name="username" type="text" required>
-    </div>
-    <div>
-        <label for="password">Mot de passe:</label>
-        <input name="password" type="password" required>
-    </div>
-
-    <input type="submit" value="Connexion">
-</form>
